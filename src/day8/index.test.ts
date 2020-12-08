3498178fdf47b529f98a07d83048a4b826d6512e
TheDartCode <giorgos@thedartcode.com>
2020-12-08 12:18:23 +0200

    Solve Day8

--- src/day8/index.test.ts
+++ src/day8/index.test.ts
@@ -1,16 +1,24 @@
 import day8 from "./index";
 
-const TEST_DATA = '';
+const TEST_DATA = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`;
 
 describe("day8", () => {
   describe("first part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day8('a', TEST_DATA)).toBe(2);
+    it("satisfies test data", () => {
+      expect(day8('a', TEST_DATA)).toBe("5");
     });
   });
   describe("second part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day8('b', TEST_DATA)).toBe(1);
+    it("satisfies test data", () => {
+      expect(day8('b', TEST_DATA)).toBe("8");
     });
   });
 });
