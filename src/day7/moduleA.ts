466c5372e192a1eb615cbc2a6aba19ea3d46496d
TheDartCode <giorgos@thedartcode.com>
2020-12-08 11:44:30 +0200

    Solve Day7

--- src/day7/moduleA.ts
+++ src/day7/moduleA.ts
@@ -1,5 +1,30 @@
-const moduleA = () => {
-  return null;
+import { constructBagsGraph, Graph } from "./helpers";
+
+const traverseGraph = (
+  graph: Graph,
+  startingColor: string,
+  desiredColor: string
+): boolean => {
+  for (let content of graph[startingColor].contents) {
+    if (content.color === desiredColor) {
+      return true;
+    }
+    if (traverseGraph(graph, content.color, desiredColor)) {
+      return true;
+    }
+  }
+};
+
+const moduleA = (list) => {
+  const rules = list.split("\n").filter((l) => l.length > 0);
+  const graph = constructBagsGraph(rules);
+  let validColorsCount = 0;
+  Object.keys(graph).forEach((color) => {
+    if (traverseGraph(graph, color, "shiny gold")) {
+      validColorsCount++;
+    }
+  });
+  return validColorsCount;
 };
 
 export default moduleA;
