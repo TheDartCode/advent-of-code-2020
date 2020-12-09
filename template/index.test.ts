c3da85137911f9893b6aa50c3e8a1176239f8b64
TheDartCode <giorgos@thedartcode.com>
2020-12-09 13:12:10 +0200

    Reformat Template files

--- template/index.test.ts
+++ template/index.test.ts
@@ -5,12 +5,12 @@ const TEST_DATA = ``;
 describe("dayX", () => {
   describe("first part", () => {
     it.skip("satisfies test data", () => {
-      expect(dayX('a', TEST_DATA)).toBe("2");
+      expect(dayX("a", TEST_DATA)).toBe("2");
     });
   });
   describe("second part", () => {
     it.skip("satisfies test data", () => {
-      expect(dayX('b', TEST_DATA)).toBe("1");
+      expect(dayX("b", TEST_DATA)).toBe("1");
     });
   });
 });
