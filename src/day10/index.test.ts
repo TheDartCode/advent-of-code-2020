c292f26c47163bca3b8273d2493ff2bbcfb481b6
TheDartCode <giorgos@thedartcode.com>
2020-12-11 23:57:36 +0200

    Solve Day10

--- src/day10/index.test.ts
+++ src/day10/index.test.ts
@@ -1,16 +1,67 @@
 import day10 from "./index";
+import { calculateJoltageDifferences } from "./moduleA";
 
-const TEST_DATA = ``;
+const TEST_DATA_SIMPLE = `16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`;
+
+const TEST_DATA = `28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`;
 
 describe("day10", () => {
+  describe("calculateJoltageDifferences", () => {
+    it("returns an object with counts of joltage differences", () => {
+      expect(calculateJoltageDifferences(TEST_DATA)).toEqual({
+        "1": 22,
+        "3": 10,
+      });
+    });
+  });
   describe("first part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day10("a", TEST_DATA)).toBe("2");
+    it("satisfies test data", () => {
+      expect(day10("a", TEST_DATA)).toBe("220");
     });
   });
   describe("second part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day10("b", TEST_DATA)).toBe("1");
+    it("satisfies test data", () => {
+      expect(day10("b", TEST_DATA)).toBe("19208");
     });
   });
 });
--- src/day10/moduleB.ts
+++ src/day10/moduleB.ts
@@ -1,5 +1,26 @@
-const moduleB = (list) => {
-  return list;
+const moduleB = (list: string): number => {
+  const entries = list
+    .split("\n")
+    .filter((l) => l.length > 0)
+    .map((l) => parseInt(l))
+    .sort((a, b) => a - b);
+  entries.push(entries[entries.length - 1] + 3);
+  entries.unshift(0);
+
+  const variantsPerStep = [1];
+
+  entries.forEach((entry, index) => {
+    let nextIndex = index + 1;
+    while (entries[nextIndex] <= entry + 3) {
+      if (!variantsPerStep[nextIndex]) {
+        variantsPerStep[nextIndex] = 0;
+      }
+      variantsPerStep[nextIndex] += variantsPerStep[index];
+      nextIndex++;
+    }
+  })
+
+  return variantsPerStep[variantsPerStep.length - 1];
 };
 
 export default moduleB;
