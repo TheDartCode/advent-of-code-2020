2d8feb12cb913aa4854618a3891fdecfe4557eea
TheDartCode <giorgos@thedartcode.com>
2020-12-12 13:11:59 +0200

    Solve Day12

--- src/day12/index.test.ts
+++ src/day12/index.test.ts
@@ -1,16 +1,76 @@
 import day12 from "./index";
+import { getNextShipState, getNextWaypointState, rotate } from "./moduleB";
+import { Action } from "./types";
 
-const TEST_DATA = ``;
+const TEST_DATA = `F10
+N3
+F7
+R90
+F11`;
 
 describe("day12", () => {
   describe("first part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day12("a", TEST_DATA)).toBe("2");
+    it("satisfies test data", () => {
+      expect(day12("a", TEST_DATA)).toBe("25");
     });
   });
   describe("second part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day12("b", TEST_DATA)).toBe("1");
+    describe("#getNextShipState", () => {
+      it("moves forward", () => {
+        expect(
+          getNextShipState(
+            { x: 0, y: 0, bearingIndex: 1 },
+            { x: -10, y: 1 },
+            { action: Action.Forward, amount: 10 }
+          )
+        ).toEqual({
+          bearingIndex: 1,
+          x: -100,
+          y: 10,
+        });
+        expect(
+          getNextShipState(
+            { x: -170, y: 38, bearingIndex: 1 },
+            { x: -4, y: -10 },
+            { action: Action.Forward, amount: 11 }
+          )
+        ).toEqual({
+          bearingIndex: 1,
+          x: -214,
+          y: -72,
+        });
+      });
+    });
+    describe("rotate", () => {
+      it("rotates based on current position, direction, and degrees", () => {
+        expect(rotate(Action.Right, 90, { x: -10, y: 4 })).toEqual({
+          x: -4,
+          y: -10,
+        });
+        expect(rotate(Action.Right, 90, { x: 0, y: 4 })).toEqual({
+          x: -4,
+          y: -0,
+        });
+        expect(rotate(Action.Left, 90, { x: 10, y: 4 })).toEqual({
+          x: 4,
+          y: -10,
+        });
+        expect(rotate(Action.Left, 90, { x: 0, y: 4 })).toEqual({
+          x: 4,
+          y: 0,
+        });
+        expect(rotate(Action.Left, 180, { x: 0, y: 4 })).toEqual({
+          x: 0,
+          y: -4,
+        });
+        expect(rotate(Action.Right, 360, { x: 0, y: 4 })).toEqual({
+          x: 0,
+          y: 4,
+        });
+      });
+    });
+    it("satisfies test data", () => {
+      expect(day12("b", TEST_DATA)).toBe("286");
     });
   });
 });
--- src/day12/moduleA.ts
+++ src/day12/moduleA.ts
@@ -1,5 +1,68 @@
-const moduleA = (list) => {
-  return list;
+import { parseInstruction } from "./helpers";
+import { Action, Instruction, ShipInfo, BEARINGS } from "./types";
+
+const getNextShipState = (
+  initialState: ShipInfo,
+  instruction: Instruction
+): ShipInfo => {
+  const result = {
+    x: initialState.x,
+    y: initialState.y,
+    bearingIndex: initialState.bearingIndex,
+  };
+
+  let action = instruction.action;
+  const { amount } = instruction;
+  if (action === Action.Forward) {
+    action = BEARINGS[initialState.bearingIndex];
+  }
+  switch (action) {
+    case Action.North:
+      result.y += amount;
+      break;
+    case Action.West:
+      result.x += amount;
+      break;
+    case Action.South:
+      result.y -= amount;
+      break;
+    case Action.East:
+      result.x -= amount;
+      break;
+    case Action.Right:
+      result.bearingIndex =
+        (result.bearingIndex + amount / 90) % BEARINGS.length;
+      break;
+    case Action.Left:
+      result.bearingIndex =
+        (BEARINGS.length + result.bearingIndex - amount / 90) % BEARINGS.length;
+      break;
+    default:
+      throw new Error(
+        `Unknown action \`${action}\`, ${JSON.stringify(instruction)}`
+      );
+  }
+
+  return result;
+};
+
+const moduleA = (list: string): number => {
+  const instructions: Instruction[] = list
+    .split("\n")
+    .filter((l) => l.length > 0)
+    .map((l: string) => parseInstruction(l));
+
+  let currentState: ShipInfo = {
+    x: 0,
+    y: 0,
+    bearingIndex: 1,
+  };
+
+  instructions.forEach((instruction) => {
+    currentState = getNextShipState(currentState, instruction);
+  });
+
+  return Math.abs(currentState.x) + Math.abs(currentState.y);
 };
 
 export default moduleA;
