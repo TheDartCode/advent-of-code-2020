038df5dd77155a7ac1281271be978a65d85d7b87
TheDartCode <giorgos@thedartcode.com>
2020-12-15 10:51:25 +0200

    Solve Day15:A

--- src/day15/index.test.ts
+++ src/day15/index.test.ts
@@ -4,8 +4,8 @@ const TEST_DATA = `0,3,6`;
 
 describe("day15", () => {
   describe("first part", () => {
-    it.skip("satisfies test data", () => {
-      expect(day15("a", TEST_DATA)).toBe("2");
+    it("satisfies test data", () => {
+      expect(day15("a", TEST_DATA)).toBe("436");
     });
   });
   describe("second part", () => {
--- src/day15/moduleA.ts
+++ src/day15/moduleA.ts
@@ -1,5 +1,24 @@
-const moduleA = (list) => {
-  return list;
+const moduleA = (list: string): number => {
+  const startingNumbers = list.split(",").map((c) => parseInt(c));
+  const lastAppearances = new Array();
+  const nM2Appearances = new Array();
+  let previousNum: number;
+  for (let turn = 1; turn <= 2020; turn++) {
+    let currentNum: number;
+    if (turn <= startingNumbers.length) {
+      currentNum = startingNumbers[turn - 1];
+    } else {
+      currentNum = nM2Appearances[previousNum]
+        ? turn - 1 - nM2Appearances[previousNum]
+        : 0;
+    }
+
+    nM2Appearances[currentNum] = lastAppearances[currentNum];
+    lastAppearances[currentNum] = turn;
+
+    previousNum = currentNum;
+  }
+  return previousNum;
 };
 
 export default moduleA;
