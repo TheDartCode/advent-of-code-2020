0d2993cafbe007b1ba7d4b0d0c6e0326028e24ac
TheDartCode <giorgos@thedartcode.com>
2020-12-16 10:30:36 +0200

    Bootstrap Day16

---
+++ src/day16/index.test.ts
@@ -0,0 +1,16 @@
+import day16 from "./index";
+
+const TEST_DATA = ``;
+
+describe("day16", () => {
+  describe("first part", () => {
+    it.skip("satisfies test data", () => {
+      expect(dayX("a", TEST_DATA)).toBe("2");
+    });
+  });
+  describe("second part", () => {
+    it.skip("satisfies test data", () => {
+      expect(day16("b", TEST_DATA)).toBe("1");
+    });
+  });
+});
