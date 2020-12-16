292cfef3718e0e3f177e92b6299ebb17f7a57e07
TheDartCode <giorgos@thedartcode.com>
2020-12-16 11:40:24 +0200

    Update bootstrap script

--- scripts/bootstrap_day.sh
+++ scripts/bootstrap_day.sh
@@ -8,3 +8,4 @@ touch "data/day$DAY.txt";
 sed -e "s/\(import day$PREVDAY from \".\/day$PREVDAY\";\)/\1 import day$DAY from \".\/day$DAY\";/g" -i "" src/app.ts
 sed -e "s/\(  \"$PREVDAY\": day$PREVDAY,\)/\1 \"$DAY\": day$DAY/g" -i "" src/app.ts
 ./node_modules/.bin/prettier -w src/app.ts
+sed -i "" -- "s/dayX/day$DAY/g" "src/day$DAY/*"
