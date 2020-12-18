d63e0119ec702a777ac92e610fb7fc6d34efedce
TheDartCode <giorgos@thedartcode.com>
2020-12-18 18:21:30 +0200

    Add mode of generic solver

--- src/app.ts
+++ src/app.ts
@@ -52,14 +52,22 @@ const part = process.argv[3] as DayPart;
 
 console.log({ day, part });
 
-const data = fs
-  .readFileSync(path.join(__dirname, `../data/day${day}.txt`))
-  .toString();
+const solveDayPart = (day: string, part: DayPart) => {
+  const data = fs
+    .readFileSync(path.join(__dirname, `../data/day${day}.txt`))
+    .toString();
+  const pm = new PerformanceMonitor();
+  const result = days[day](part, data);
+  const time = pm.tick();
+  console.log(`Day ${day} ${part}`, { result });
+  console.log(`Done in ${time.toFixed(2)}ms`);
+};
 
-const pm = new PerformanceMonitor();
-
-const result = days[day](part, data);
-const time = pm.tick();
-
-console.log({ result });
-console.log(`Done in ${time.toFixed(2)}ms`);
+if (day && part) {
+  solveDayPart(day, part);
+} else {
+  Object.keys(days).forEach((day) => {
+    solveDayPart(day, DayPart.a);
+    solveDayPart(day, DayPart.b);
+  });
+}
