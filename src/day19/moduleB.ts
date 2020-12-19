0e9e7946299dcbe044b351692f2e699f0a3a4e7f
TheDartCode <giorgos@thedartcode.com>
2020-12-19 12:31:26 +0200

    Solve Day19:B

--- src/day19/moduleB.ts
+++ src/day19/moduleB.ts
@@ -1,5 +1,14 @@
-const moduleB = (list) => {
-  return list;
+import { parseInput } from "./helpers";
+
+const moduleB = (input: string): number => {
+  input = input
+    .replace("8: 42", "8: 42 | 42 8")
+    .replace("11: 42 31", "11: 42 31 | 42 11 31");
+  const { rules, messages } = parseInput(input);
+
+  const masterRegExp = new RegExp(`^${rules[0].getRegexString()}$`);
+
+  return messages.filter((message) => masterRegExp.test(message)).length;
 };
 
 export default moduleB;
--- src/day19/types.ts
+++ src/day19/types.ts
@@ -1,27 +1,39 @@
+const MAX_RECURSION_DEPTH = 7;
+
 export type RuleComposition = Rule[];
 
 export type RuleContents = string | RuleComposition[];
 
 export class Rule {
   public contents: RuleContents;
-  public parents: Set<Rule> = new Set();
 
   constructor(public id: number) {}
 
-  public getRegexString(): string {
+  public getRegexString(depth: number = 0): string {
+    if (depth > MAX_RECURSION_DEPTH) {
+      return "";
+    }
     if (typeof this.contents === "string") {
       return this.contents;
     }
 
     if (this.contents.length === 1) {
       return Rule.createRegexGroup(
-        this.contents[0].map((cc) => cc.getRegexString()).join("")
+        this.contents[0]
+          .map((cc) => cc.getRegexString(cc.id === this.id ? depth + 1 : depth))
+          .join("")
       );
     }
 
     return Rule.createRegexGroup(
       this.contents
-        .map((c) => c.map((cc) => cc.getRegexString()).join(""))
+        .map((c) =>
+          c
+            .map((cc) =>
+              cc.getRegexString(cc.id === this.id ? depth + 1 : depth)
+            )
+            .join("")
+        )
         .join("|")
     );
   }
